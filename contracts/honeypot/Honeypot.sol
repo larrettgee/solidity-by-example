76966a6ad454eb81fa1267ee78dc011ca92be561
Garrett Lee <garr.c.lee@gmail.com>
2024-08-07 11:36:10 -0500

    simple honeypot

---
+++ contracts/honeypot/Honeypot.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Bank {
+    mapping(address => uint256) public balances;
+    Logger logger;
+
+    constructor(Logger _logger) {
+        logger = Logger(_logger);
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        logger.log(msg.sender, msg.value, "Deposit");
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(_amount <= balances[msg.sender], "Insufficient funds");
+
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] -= _amount;
+
+        logger.log(msg.sender, _amount, "Withdraw");
+    }
+}
+
+contract Logger {
+    event Log(address caller, uint256 amount, string action);
+
+    function log(address _caller, uint256 _amount, string memory _action) public {
+        emit Log(_caller, _amount, _action);
+    }
+}
+
+contract HoneyPot {
+    function log(address _caller, uint256 _amount, string memory _action) public {
+        if (equal(_action, "Withdraw")) {
+            revert("It's a trap");
+        }
+    }
+
+    // Function to compare strings using keccak256
+    function equal(string memory _a, string memory _b) public pure returns (bool) {
+        return keccak256(abi.encode(_a)) == keccak256(abi.encode(_b));
+    }
+}
