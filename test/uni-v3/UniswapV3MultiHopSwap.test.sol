72620b5165ea895ee93379eda4028816a51f951f
Garrett Lee <garr.c.lee@gmail.com>
2024-08-17 09:13:20 -0500

    uniswap swaps

--- test/uni-v3/UniswapV3MultiHopSwap.test.sol
+++ test/uni-v3/UniswapV3MultiHopSwap.test.sol
@@ -2,36 +2,35 @@
 pragma solidity ^0.8.24;
 
 import {Test, console2} from "../../lib/forge-std/src/Test.sol";
-import "../../contracts/uni-v3/UniswapV3SingleHopSwap.sol";
+import "../../contracts/uni-v3/UniswapV3MultiHopSwap.sol";
 
-contract UniswapV3SingleHopSwapTest is Test {
-    address private constant DAI_WETH_POOL_3000 = 0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8;
-
+contract UniswapV3MultiHopSwapTest is Test {
     IWETH private constant weth = IWETH(WETH);
     IERC20 private constant dai = IERC20(DAI);
+    IERC20 private constant usdc = IERC20(USDC);
 
-    UniswapV3SingleHopSwap private swap;
+    UniswapV3MultiHopSwap private swap;
 
-    uint256 private constant AMOUNT_IN = 1e18;
-    uint256 private constant AMOUNT_OUT = 50 * 1e18;
+    uint256 private constant AMOUNT_IN = 10 * 1e18;
+    uint256 private constant AMOUNT_OUT = 20 * 1e18;
     uint256 private constant MAX_AMOUNT_IN = 1e18;
 
     function setUp() public {
-        swap = new UniswapV3SingleHopSwap();
+        swap = new UniswapV3MultiHopSwap();
         weth.deposit{value: AMOUNT_IN + MAX_AMOUNT_IN}();
         weth.approve(address(swap), type(uint256).max);
     }
 
-    function test_swapExactInputSingleHop() public {
-        swap.swapExactInputSingleHop(AMOUNT_IN, 1);
+    function test_swapExactInputMultiHop() public {
+        swap.swapExactInputMultiHop(AMOUNT_IN, 1);
         uint256 d1 = dai.balanceOf(address(this));
         assertGt(d1, 0, "DAI balance = 0");
     }
 
-    function test_swapExactOutputSingleHop() public {
+    function test_swapExactOutputMultiHop() public {
         uint256 w0 = weth.balanceOf(address(this));
         uint256 d0 = dai.balanceOf(address(this));
-        swap.swapExactOutputSingleHop(AMOUNT_OUT, MAX_AMOUNT_IN);
+        swap.swapExactOutputMultiHop(AMOUNT_OUT, MAX_AMOUNT_IN);
         uint256 w1 = weth.balanceOf(address(this));
         uint256 d1 = dai.balanceOf(address(this));
 
